a82d6049fe3b7028f4e82d5f081840f08851a078
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2025-01-02 00:38:38 +0200

    modified:   src/components/Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,7 +31,7 @@ let navLinks = [
     <nav  class='bg-[#ffffff00] py-5 relative '>
       <div class="justify-around flex container m-auto ">
         <div class='flex gap-2 items-center'> 
-        <ul class="z-50 flex lg:flex-row flex-col gap-4 text-lg	font-normal	 lg:static lg:w-auto lg:bg-transparent absolute left-0 bg-white w-full ${toggleMenu ? 'h-80' : 'h-0'} lg:h-auto duration-500 overflow-hidden top-full lg:shadow-none shadow-md">
+        <ul class="z-50 flex flex-row  gap-4 text-lg	font-normal	 lg:static lg:w-auto lg:bg-transparent absolute left-0 bg-white w-full ${toggleMenu ? 'h-80' : 'h-0'} lg:h-auto duration-500 overflow-hidden top-full lg:shadow-none shadow-md">
         
         <li v-for="(link, index) in navLinks" class=' flex items-center cursor-pointer' :key="index">
           <RouterLink :to="link.pathname" :class="[ isActiveLink(link.pathname) ? 'bg-gray-700' : '' ,'text-secondary font-bold hover:text-primary transition-all duration-200 px-2 py-1']">{{$t(link.navName)}}</RouterLink>
@@ -41,7 +41,7 @@ let navLinks = [
       </div>
       </div>
     </nav>
-    <div class=" flex gap-7 justify-center">
+    <div class=" flex gap-7 mt-10 justify-center">
     <button @click="changeLanguage('en')" class=" bg-green-700 p-2 rounded-lg font-bold text-white">English</button>
     <button @click="changeLanguage('ar')" class=" bg-red-700 p-2 rounded-lg font-bold text-white">عربي</button>
   </div>
